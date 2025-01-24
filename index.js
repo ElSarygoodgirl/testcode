41499a6c88ec4caf8cc6913e4b5f4ccd6a2fc82c
sary <el.sary@institute.pse.ngo>
2025-01-24 13:53:50 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const display = document.getElementById("display");
+function appeendToDisplay(input) {
+  display.value += input;
+}
+function clearDisplay() {
+  display.value = "";
+}
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Erorr";
+  }
+}
